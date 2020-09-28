ae3b7a851a85e3e0f47976592627985e8eace70e
IvanKazakov2352 <testkazkov@mail.ru>
2020-09-28 10:02:34 +0300

    fix toolbar editor

--- client/src/components/CourseComponents/CourseModule/ModuleCreateLectionAndTest.vue
+++ client/src/components/CourseComponents/CourseModule/ModuleCreateLectionAndTest.vue
@@ -86,6 +86,22 @@ export default {
       placeholder: "Создание лекции",
       readOnly: true,
       theme: "snow",
+      modules: {
+        toolbar: [
+          ["bold", "italic", "underline", "strike"],
+          ["blockquote", "code-block"],
+          [{ header: 1 }, { header: 2 }],
+          [{ list: "ordered" }, { list: "bullet" }],
+          [{ script: "sub" }, { script: "super" }],
+          [{ indent: "-1" }, { indent: "+1" }],
+          [{ direction: "rtl" }],
+          [{ size: ["small", false, "large", "huge"] }],
+          [{ header: [1, 2, 3, 4, 5, 6, false] }],
+          [{ color: [] }, { background: [] }],
+          [{ font: [] }],
+          [{ align: [] }],
+        ],
+      },
     },
   }),
   methods: {
